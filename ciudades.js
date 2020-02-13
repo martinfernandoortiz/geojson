a0a979f7ac1273e24e219e2a6167a1a9d2fefe8c
martinfernandoortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2020-02-13 16:58:00 -0300

    Add files via upload

---
+++ ciudades.js
@@ -0,0 +1,16 @@
+var json_ciudades = {
+"type": "FeatureCollection",
+"name": "ciudades",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Nombre": "Mar del Plata" }, "geometry": { "type": "Point", "coordinates": [ -57.5482703, -37.9977225 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Nombre": "Mendoza Capital" }, "geometry": { "type": "Point", "coordinates": [ -68.8442956, -32.8897294 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Nombre": "San Fernando del Valle" }, "geometry": { "type": "Point", "coordinates": [ -65.7790719, -28.4689063 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Nombre": "San Salvador de Jujuy" }, "geometry": { "type": "Point", "coordinates": [ -65.299478900000011, -24.1852569 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Nombre": "Salta Capital" }, "geometry": { "type": "Point", "coordinates": [ -65.4103194, -24.789294600000012 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Nombre": "Corrientes Capital" }, "geometry": { "type": "Point", "coordinates": [ -58.765785322402635, -27.526748250000011 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Nombre": "Santa Fé Capital" }, "geometry": { "type": "Point", "coordinates": [ -60.643035777013843, -31.64727905 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "Nombre": "Córdoba Capital" }, "geometry": { "type": "Point", "coordinates": [ -64.183319, -31.417339100000014 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "Nombre": "San Rafael" }, "geometry": { "type": "Point", "coordinates": [ -68.330514, -34.6126025 ] } }
+]
+}
